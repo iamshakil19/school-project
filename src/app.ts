cb4fed6aeab2a19d3d4f04f8590334a547732b5a
iamshakil19 <shakilahmed.pure@gmail.com>
2023-06-13 21:08:01 +0600

    project setup almost done

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import cors from "cors";
+import express, { Application, NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import routes from "./app/routes";
+
+const app: Application = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Application route
+app.use("/api/v1/", routes);
+
+// Testing
+app.get("/", (req: Request, res: Response) => {
+  res.send("School Working Successfully");
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Not found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "Api Not Found",
+      },
+    ],
+  });
+
+  next();
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+import { errorLogger, logger } from "./shared/logger";
+
+process.on("uncaughtException", (error) => {
+  errorLogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(`🆗 Database are connected`);
+
+    server = app.listen(config.port, () => {
+      logger.info(`School app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    errorLogger.error(`❌ Failed to connect database: ${error}`);
+  }
+
+  process.on("unhandledRejection", (error) => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+main();
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM is received");
+  if (server) {
+    server.close();
+  }
+});
